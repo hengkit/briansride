a723fe0b73bc9d2f02eca3aa9c40b3b0b44ca19e
Kit <kit@getpantheon.com>
2016-09-08 11:36:05 -0700

    moving stuff around

---
+++ maps.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([41.876209, -87.619057], 10);
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.name) {
+      layer.bindPopup(feature.geometry.type+feature.properties.name);
+    }
+  }
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors '
+
+}).addTo(map);
+
+L.geoJson(r66,{
+  onEachFeature: onEachFeature
+}).addTo(map);
